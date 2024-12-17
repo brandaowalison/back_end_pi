1a86b201b712080e25d56f5d67d3544e62dfd697
Walison Brandão <walisonbrandao30@gmail.com>
2024-12-17 09:20:12 -0300

    main

--- back_end/server.ts
+++ back_end/server.ts
@@ -5,8 +5,7 @@ const app: Application = express();
 const server: Server = new Server(app);
 const PORT: number = 8080;
 
-app
-    .listen(PORT, "localhost", function () {
+app.listen(PORT, "localhost", function () {
         console.log(`Server is running on port ${PORT}.`);
     })
     .on("error", (err: any) => {
